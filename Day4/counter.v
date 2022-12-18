05af510ff6df4ab81f9769d3cf0f2d67c3a6d079
Narsepalli Pradyumna <npradyu@gmail.com>
2022-12-18 22:24:46 +0530

    Adding Day4 drafts and removing unnecessary items of Day3

---
+++ Day4/counter.v
@@ -0,0 +1,27 @@
+module up_counter    (
+out     ,  // Output of the counter
+enable  ,  // enable for counter
+clk     ,  // clock Input
+reset      // reset Input
+);
+
+output [3:0] out;
+//you can alternately write this as output reg [7:0] out;
+input enable, clk, reset;
+//------------Internal Variables--------
+reg [3:0] out; 
+
+
+
+always @(posedge clk)
+if (reset) begin //reset ==1
+  out = 4'b0 ;
+end 
+else if (en
+able) begin //reset =0
+  out = out + 1;
+end
+
+endmodule 
+
+
